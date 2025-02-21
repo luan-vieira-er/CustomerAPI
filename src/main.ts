100de891fe00a45f92fe2cd031f772dcf0e67da6
Luan <luan.vieira@avmb.com.br>
2025-02-21 09:01:53 -0300

    CORS

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,12 @@ async function bootstrap() {
   });
 
   app.setGlobalPrefix('api/v1');
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
 
   app.useGlobalPipes(
     new ValidationPipe({
